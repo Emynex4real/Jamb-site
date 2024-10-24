c71ec366122c47ba8635c915e3c78b512a70a451
Emynex4real <122555067+Emynex4real@users.noreply.github.com>
2024-10-24 12:55:24 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,15 @@
+const share = document.querySelector('.fa-share');
+const end = document.getElementById('share');
+const social = document.querySelector('.social')
+
+
+share.addEventListener('click', ()=>{
+    console.log('hello')
+    social.classList.toggle('active')
+    
+    
+})
+
+end.addEventListener('click', ()=>{
+    social.classList.remove('active')
+})
